4d75392795629f3648ec54f8628c53c53855cc65
Gabriel Weyer <gabrielweyer@gmail.com>
2022-01-15 11:15:54 +1100

    Use Karma coverage instead of istanbul

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/2.0/config/configuration-file.html
 
-const path = require('path');
-
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
@@ -16,24 +14,27 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: path.join(__dirname, 'coverage'),
-      reports: [ 'html', 'cobertura' ],
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      dir: 'coverage/',
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'cobertura' }
+      ]
     },
     junitReporter: {
       outputDir: 'tests-results',
       outputFile: 'tests-results.xml',
       useBrowserName: false
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
+    reporters: ['progress', 'kjhtml', 'junit', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
