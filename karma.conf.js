054f5eceeb3a0ba9bda462c367effcded0120827
Gabriel Weyer <gabrielweyer@gmail.com>
2022-04-17 19:06:25 +1000

    Upload test results as HTML

--- karma.conf.js
+++ karma.conf.js
@@ -14,12 +14,15 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-htmlfile-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-junit-reporter')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    htmlReporter: {
+      outputFile: 'test-results/test-results.html'
     },
     coverageReporter: {
       dir: 'coverage/',
@@ -28,12 +31,7 @@ module.exports = function (config) {
         { type: 'html' }
       ]
     },
-    junitReporter: {
-      outputDir: 'tests-results',
-      outputFile: 'tests-results.xml',
-      useBrowserName: false
-    },
-    reporters: ['progress', 'kjhtml', 'junit', 'coverage'],
+    reporters: ['progress', 'kjhtml', 'html', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
